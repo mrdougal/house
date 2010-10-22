aa5362f3e16e787715ddccbf698128ec2b6c07c1
Dougal MacPherson <hello@newfangled.com.au>
2010-10-22 15:53:24 +1100

    Validations for zero byte files

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -12,14 +12,14 @@ class Asset
   
   field :name
   field :original_filename                        # filename
-  field :size, :type => Integer, :default => 0    # filesize in bytes
+  field :original_filesize, :type => Integer, :default => 0    # filesize in bytes
   
   field :preview_generated_at, :type => DateTime
   
+  # attr_accessor :file
+  # validates_presence_of :file, :on => :create, :message => "is required" 
   
-  
-  attr_accessor :file
-  validate :check_have_file
+  validate :check_file
   
   
   # Callbacks
@@ -33,7 +33,7 @@ class Asset
   
 
   def name
-    @name || basename
+    self[:name].blank? ?  basename : self[:name]
   end
   
   # This is where the file (from the filesystem) is assigned to the record
@@ -41,14 +41,29 @@ class Asset
   # Additional metadata will be extracted at a later date
   # 
   def file=(file)
-
+  
     @file = file
-
+  
     # Set the original_filename
     self.original_filename = get_original_filename
-
-    # Get the size of the file
-    self.size = get_filesize
+  
+    # Set the size of the file
+    self.original_filesize = get_filesize
+  end
+  
+  # alias :size :original_filesize
+  
+  
+  # Returns our original file
+  # TODO move path and url methods into this object
+  def file
+    @file
+    # return nil unless path
+    # File.new(path)
+  end
+  
+  def file?
+    !!@file
   end
 
 
@@ -63,32 +78,51 @@ class Asset
   
   
   def get_original_filename
+    
+    # If file hasn't been set return nil
+    return nil unless file?
+    
     @file.respond_to?(:original_filename) ? @file.original_filename : File.basename(@file.path)
   end
 
+
   def get_filesize
-    File.size(@file)
+
+    # We need to have a file
+    return unless file?
+
+    # get the size of the file
+    File.size(file) 
+
   end
 
   def ok_to_process?
     valid?
   end
 
-  def check_have_file
-
-    if @file.nil?
-      errors.add(:file, 'You need to upload a file')
+  # Check to see if we have a file asssigned (which is a requirement)
+  # and check that the file isn't zero bytes
+  def check_file
+    
+    if file?
+      # Add errors if the file is empty
+      errors.add(:file, 'was zero bytes') if File.size(file).zero?
+    else
+      # Add error if there is no file
+      errors.add(:file, 'You need to upload a file') unless file?
     end
   end
   
 
-  # Create our path to where we need to be stored, then moves our object there
+  # Create our path to where we need to be stored, then move our file there
   def store!
     
-    # # Construct the path in the filesystem
-    # FileUtils.mkdir_p(File.dirname(path))
-    # FileUtils.mv(file.path, path)
-    # FileUtils.chmod(0644, path)
+    raise "Unable to store the file" unless @file
+    
+    # Construct the path in the filesystem
+    FileUtils.mkdir_p(File.dirname(path))
+    FileUtils.mv(@file.path, path)
+    FileUtils.chmod(0644, path)
     
   end
 
--- spec/models/asset_spec.rb
+++ spec/models/asset_spec.rb
@@ -2,12 +2,102 @@ require 'spec_helper'
 
 describe Asset do
 
-  it "should do something" do
-    raise 'Not true'
+  describe "Creation of asset" do
+    before(:each) do
+      @asset = Asset.new :file => get_fixture("images/example.png")
+      @asset.save
+    end
+    
+    it "should have a basename" do
+      @asset.basename.should == 'example.png'
+    end
+    
+    it "should have a filesize" do
+      @asset.original_filesize.should_not be_zero
+    end
+    
   end
   
-  it "should do something else" do
-    false
+  describe "Creation of asset with empty file" do
+    before(:each) do
+      @asset = Factory.build :asset, :file => get_fixture("zero-bytes.txt") 
+      @asset.valid?
+    end
+    
+    it "should not be valid" do
+      @asset.should_not be_valid
+    end
+    
+    it "should have errors on file" do
+      @asset.should have(1).error_on(:file)
+    end
+    
+    it "should have an error on :file, mentioning zero bytes" do
+      @asset.errors[:file].to_s.should =~ /zero bytes/
+    end
+  end
+
+
+  describe "Creation of asset without file" do
+
+    before(:each) do
+      @asset = Factory.build :asset, :file => nil
+      @asset.save
+    end
+    
+    it "should not be valid" do
+      @asset.should_not be_valid
+    end
+    
+    it "should have errors" do
+      @asset.errors.should_not be_empty
+    end
+      
+    # 2010-10-19
+    # This is erroring rspec :-(
+    # output -> expected errors_on to be a collection but it does not respond to #length or #size
+    # Unless i've written a really shite spec
+    # this isn't be erroring like this
+    # might be, because we're not using activerecord
+    #
+    # Have rewritten this test below
+    # I assume that I've got a confussed copy of rspec installed
+    #
+    it "should have at least one error on file" do
+      # error_on will call valid? internally
+      @asset.should have(1).errors_on(:file)
+    end
+
+    # This method does the same check as above
+    # Although the above method is crashing rspec
+    it "should have at least one error on file (re-written)" do
+      @asset.valid?
+      @asset.errors[:file].should_not be_nil
+    end
+    
+  end
+  
+  describe "Updating an asset (without setting the file attribute)" do
+    
+    before(:each) do
+      @asset = Factory.create :asset, :file => get_fixture("images/example.png") #, :new_record => false 
+    end
+    
+    describe "updating name" do
+      before(:each) do
+        @asset.update :name => "Bacon"
+      end
+        
+      it "should be valid" do
+        @asset.should be_valid
+      end
+      
+      it "shouldn't effect the basename of the file" do
+        @asset.basename.should == 'example.png'
+      end
+      
+    end
+
   end
 
 end
