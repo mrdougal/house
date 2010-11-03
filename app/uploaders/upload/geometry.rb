4c0dfaa7dd7768938e4cb0dc357d1181dd8aa026
Dougal MacPherson <hello@newfangled.com.au>
2010-11-04 08:28:14 +1100

    Moved dimensions into a private method. As it's an expensive call.

--- app/uploaders/upload/geometry.rb
+++ app/uploaders/upload/geometry.rb
@@ -6,14 +6,60 @@ module Upload
   
     module InstanceMethods
       
+      def height
+        dimensions[:height].to_i        
+      end
+      
+      def width
+        dimensions[:width].to_i  
+      end
+
+      # True if the dimensions represent a square
+      def square?
+        height == width
+      end
+
+      # True if the dimensions represent a horizontal rectangle
+      def horizontal?
+        height < width
+      end
+      
+      # True if the dimensions represent a vertical rectangle
+      def vertical?
+        height > width
+      end
+      
+      # The aspect ratio of the dimensions.
+      def aspect
+        width.to_f / height.to_f
+      end
+
+      # Returns the larger of the two dimensions
+      def larger
+        [height, width].max
+      end
+
+      # Returns the smaller of the two dimensions
+      def smaller
+        [height, width].min
+      end
+      
+      
+      
+      private
+      
+      
       # Returns the resulting dimensions of the preview file
       #
-      # We are using sips to tell us the dimensions of the preview.
       # It would be unreliable to base the dimensions on the original asset
       # as some items won't have pixel dimensions, eg: vectors (fonts, svg)
       # or the returned preview make be smaller, such as the case with iWork docs
       # when the return preview is ~400 pixels
       #
+      # We are using sips to tell us the dimensions of the preview file.
+      # Consiquently this is an expensive call to make. 
+      # (Which is why we've made it a private method)
+      # 
       # example sips usage: sips --getProperty pixelWidth path/to/asset/preview.png
       def dimensions
 
@@ -62,49 +108,7 @@ module Upload
         )
 
       end
-
-      def height
-        dimensions[:height].to_i        
-      end
       
-      def width
-        dimensions[:width].to_i  
-      end
-
-
-      # True if the dimensions represent a square
-      def square?
-        height == width
-      end
-
-      # True if the dimensions represent a horizontal rectangle
-      def horizontal?
-        height < width
-      end
-      
-      # True if the dimensions represent a vertical rectangle
-      def vertical?
-        height > width
-      end
-      
-      # The aspect ratio of the dimensions.
-      def aspect
-        width.to_f / height.to_f
-      end
-
-      # Returns the larger of the two dimensions
-      def larger
-        [height, width].max
-      end
-
-      # Returns the smaller of the two dimensions
-      def smaller
-        [height, width].min
-      end
-      
-      
-      
-      private
       
       # Now we'll step through each line of the output from sips.
       # Example of typical output from sips...
