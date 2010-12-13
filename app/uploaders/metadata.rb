c18cd62351aa26eb53c747a6908a20df467b8d85
Dougal MacPherson <hello@newfangled.com.au>
2010-12-13 20:04:55 +1100

    Humanising metadata attributes
    Some attributes aren't displayed to the user
    Some values are changed, eg: 120 secounds becomes 2 minutes

--- app/uploaders/metadata.rb
+++ app/uploaders/metadata.rb
@@ -11,6 +11,9 @@ class Metadata
   include Mongoid::Document
   include MD::Extraction
   
+  include ActionView::Helpers::NumberHelper
+  # include ActionView::Helpers::TextHelper
+  
   embedded_in :asset, :inverse_of => :metadata
   
   before_create :extract
@@ -30,7 +33,86 @@ class Metadata
       true
   end
   
+  # Used in the display of attributes
+  def humanized_attributes
+    
+    
+    out = {}
+    attributes.each do |key, value|
+      
+      
+      next if hidden_attributes.include? key
+      
+      v = case 
+          when key =~ /page_/ then "#{value} points"
+            
+          # Portrait or landscape?
+          when key == 'orientation' then value.zero? ? 'landscape' : 'portrait'
+            
+            
+          # Break arrays into a sentence  
+          when value.instance_of?(Array) then value.to_sentence
+          
+          # Time
+          # Convert secounds into minutes hours etc
+          when key =~ /duration/ then secounds_to_words(value)
+          
+          # Don't convert years
+          when key =~ /year/ then value
+            
+          # Display numbers with delimiters
+          when (value.instance_of?(Fixnum) or value.instance_of?(Float)) then number_with_delimiter value
+
+          else value
+          end
+        
+      
+      
+      out[key.humanize] = v
+    end
+    
+    out
+    
+    
+  end
+  
   private
+  
+  
+  # A list of attributes that we don't need to show to the user
+  def hidden_attributes
+
+    %w{ display_name content_type_tree content_type }
+    
+  end
+  
+  
+  def secounds_to_words(secs)
+
+  case secs
+    when 0..60
+      pluralize secs, 'second'
+    when 60..3600
+
+      m = (secs.to_f/60).round(2)
+      m = m.floor if m.floor == m
+      
+      pluralize m ,'minute'
+      
+    else
+      
+      m = (secs.to_f/3600).round(2)
+      m = m.floor if m.floor == m
+      
+      pluralize m ,'hour'
+    end
+  end
+
+  def pluralize(number, singular)
+    "#{number} " << (number.to_i < 1 ? singular : singular.pluralize)
+    # "#{count || 0} " + ((count == 1 || count =~ /^1(\.0+)?$/) ? singular : (plural || singular.pluralize))
+    
+  end
 
 
 end
