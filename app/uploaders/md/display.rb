f6d659b97013ef40388708e7dbbabccbdfb7f9bf
Dougal MacPherson <hello@newfangled.com.au>
2010-12-13 22:36:29 +1100

    Moved methods for the display of metadata into a module
    To cleanup the code

--- app/uploaders/md/display.rb
+++ app/uploaders/md/display.rb
@@ -27,9 +27,98 @@ module MD
       # * Define a list of common attributes
       # * Those that are left over are 'special' for that type of asset
       
+      
+      # Formats and limits attributs for display to the user
+      # eg: content_type tree, isn't an attribute that would be considered useful to the user
+      def humanized_attributes
+
+
+        out = {}
+        attributes.each do |key, value|
+
+
+          next if hidden_attributes.include? key
+
+          v = case 
+              when key =~ /page_/ then "#{value} points"
+
+              # Portrait or landscape?
+              when key == 'orientation' then value.zero? ? 'landscape' : 'portrait'
+
+              # Convert secounds into minutes hours etc
+              when key =~ /duration/ then secounds_to_words(value)
+
+              # Don't convert years
+              when key =~ /year/ then value
+
+              when key =~ /sample_rate/ then "#{number_with_delimiter value.to_i} Hz"
+              when key =~ /bit_rate/ then "#{number_with_delimiter value.to_i} bps"
+
+
+
+              # Break arrays into a sentence  
+              when value.instance_of?(Array) then value.to_sentence
+
+              # Display numbers with delimiters
+              when (value.instance_of?(Fixnum) or value.instance_of?(Float)) then number_with_delimiter value
+
+
+              else value
+              end
+
+
+
+          out[key.humanize] = v
+        end
+
+        out
+
+
+      end
+
+      private
+
+
+      # A list of attributes that we don't need to show to the user
+      def hidden_attributes
+
+        %w{ display_name content_type_tree content_type _id }
+
+      end
+
+
+      def secounds_to_words(secs)
+
+        case secs
+        when 0..60
+          pluralize secs, 'second'
+        when 60..3600
+
+          m = (secs.to_f/60).round(2)
+          m = m.floor if m.floor == m
+
+          pluralize m ,'minute'
+
+        else
+
+          m = (secs.to_f/3600).round(2)
+          m = m.floor if m.floor == m
+
+          pluralize m ,'hour'
+        end
+      end
+
+
+      def pluralize(number, singular)
+        "#{number} " << (number.to_i < 1 ? singular : singular.pluralize)
+      end
+
+      
     end
     
     def self.included(receiver)
+      
+      receiver.send :include, ActionView::Helpers::NumberHelper
       receiver.send :include, InstanceMethods
     end
   end
--- app/uploaders/metadata.rb
+++ app/uploaders/metadata.rb
@@ -10,9 +10,8 @@ class Metadata
   include Candle
   include Mongoid::Document
   include MD::Extraction
+  include MD::Display
   
-  include ActionView::Helpers::NumberHelper
-  # include ActionView::Helpers::TextHelper
   
   embedded_in :asset, :inverse_of => :metadata
   
@@ -33,90 +32,7 @@ class Metadata
       true
   end
   
-  # Used in the display of attributes
-  def humanized_attributes
-    
-    
-    out = {}
-    attributes.each do |key, value|
-      
-      
-      next if hidden_attributes.include? key
-      
-      v = case 
-          when key =~ /page_/ then "#{value} points"
-            
-          # Portrait or landscape?
-          when key == 'orientation' then value.zero? ? 'landscape' : 'portrait'
-            
-          # Convert secounds into minutes hours etc
-          when key =~ /duration/ then secounds_to_words(value)
-          
-          # Don't convert years
-          when key =~ /year/ then value
-            
-          when key =~ /sample_rate/ then "#{number_with_delimiter value.to_i} Hz"
-          when key =~ /bit_rate/ then "#{number_with_delimiter value.to_i} bps"
 
-
-            
-          # Break arrays into a sentence  
-          when value.instance_of?(Array) then value.to_sentence
-
-          # Display numbers with delimiters
-          when (value.instance_of?(Fixnum) or value.instance_of?(Float)) then number_with_delimiter value
-
-
-          else value
-          end
-        
-      
-      
-      out[key.humanize] = v
-    end
-    
-    out
-    
-    
-  end
-  
-  private
-  
-  
-  # A list of attributes that we don't need to show to the user
-  def hidden_attributes
-
-    %w{ display_name content_type_tree content_type _id }
-    
-  end
-  
-  
-  def secounds_to_words(secs)
-
-    case secs
-    when 0..60
-      pluralize secs, 'second'
-    when 60..3600
-
-      m = (secs.to_f/60).round(2)
-      m = m.floor if m.floor == m
-    
-      pluralize m ,'minute'
-    
-    else
-    
-      m = (secs.to_f/3600).round(2)
-      m = m.floor if m.floor == m
-    
-      pluralize m ,'hour'
-    end
-  end
-
-  def pluralize(number, singular)
-    "#{number} " << (number.to_i < 1 ? singular : singular.pluralize)
-    # "#{count || 0} " + ((count == 1 || count =~ /^1(\.0+)?$/) ? singular : (plural || singular.pluralize))
-    
-  end
 
 
 end
