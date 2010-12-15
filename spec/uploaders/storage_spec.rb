e8e7596844952d44f2ad5563537469feb8187688
Dougal MacPherson <hello@newfangled.com.au>
2010-12-15 12:12:19 +1100

    Tests on storing the file within the filesystem

--- spec/uploaders/storage_spec.rb
+++ spec/uploaders/storage_spec.rb
@@ -2,6 +2,10 @@ require "spec_helper"
 
 describe "Storage of the file" do
   
+  
+  before(:each) do
+    @asset = Factory :asset
+  end
 
   
   describe "creation of the file system heriachy" do
@@ -9,22 +13,22 @@ describe "Storage of the file" do
     describe "path" do
       
       it "should calculate the path for the file" do
-        pending
+        @asset.path.should_not be_blank
       end
       
       it "should have 'original' in the files path" do
-        pending
+        @asset.path.should =~ /original/
       end
     
     end
     
     
     it "should build the path for the file" do
-      pending
+      File.exist? @asset.basepath
     end
     
     it "should move the file into a it's path" do
-      pending
+      File.exist? @asset.path
     end
     
   end
