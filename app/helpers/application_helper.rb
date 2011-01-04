81bc95b35a5d336ec2ceed930fc0b3e023649be6
Dougal MacPherson <hello@newfangled.com.au>
2011-01-04 16:16:01 +1100

    Adding in page title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,15 @@
 module ApplicationHelper
   
 
+  def title(page_title, show_title = true)
+    content_for(:title) { page_title.to_s }
+    @show_title = show_title
+  end
+  
+  def show_title?
+    @show_title
+  end
+
   def nav
     
   end
